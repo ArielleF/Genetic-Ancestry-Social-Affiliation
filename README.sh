bf277005f4fea2adf96640589c68d94b2614067e
ArielleF <afogel29@gmail.com>
2020-03-18 18:24:49 -0400

    Update README.sh

--- README.sh
+++ README.sh
@@ -7,7 +7,7 @@ sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 groom_noC_repstate1_recoded_gl
 
 sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 groom_noC_repstate2_recoded_glmmtmb.R 
 
-cat GLMMTMB_permuted_results_prox1* >> all.GLMMTMB_permuted_results_prox1_noC_repstate.txt
-cat GLMMTMB_permuted_results_prox2* >> all.GLMMTMB_permuted_results_prox2_noC_repstate.txt
-cat GLMMTMB_permuted_results_groom1* >> all.GLMMTMB_permuted_results_groom1_noC_repstate.txt
-cat GLMMTMB_permuted_results_groom2* >> all.GLMMTMB_permuted_results_groom2_noC_repstate.txt
+cat GLMMTMB_permuted_results_prox1* >> all.GLMMTMB_permuted_results_prox1_noC_repstate_recoded.txt
+cat GLMMTMB_permuted_results_prox2* >> all.GLMMTMB_permuted_results_prox2_noC_repstate_recoded.txt
+cat GLMMTMB_permuted_results_groom1* >> all.GLMMTMB_permuted_results_groom1_noC_repstate_recoded.txt
+cat GLMMTMB_permuted_results_groom2* >> all.GLMMTMB_permuted_results_groom2_noC_repstate_recoded.txt
