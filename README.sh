bb2044794a175fd4d61053bb0de47a0023d8d951
ArielleF <afogel29@gmail.com>
2020-03-18 16:05:26 -0400

    Update README.sh

--- README.sh
+++ README.sh
@@ -2,3 +2,7 @@
 sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 prox_noC_repstate1_recoded_glmmtmb.R 
 
 sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 prox_noC_repstate2_recoded_glmmtmb.R 
+
+sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 groom_noC_repstate1_recoded_glmmtmb.R 
+
+sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 groom_noC_repstate2_recoded_glmmtmb.R 
