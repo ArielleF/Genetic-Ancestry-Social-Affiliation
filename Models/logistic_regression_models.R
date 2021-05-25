4d21f79026794239d8a92ee2525de16e9d63c447
ArielleF <afogel29@gmail.com>
2021-05-25 13:31:34 -0400

    Update logistic_regression_models.R

--- Models/logistic_regression_models.R
+++ Models/logistic_regression_models.R
@@ -19,7 +19,7 @@ groom_model <- glmmTMB(groom_two_month ~ assortative_genetic_ancestry_index + he
 # look at model output (this includes non-permuted p-values)
 summary(groom_model)
 groom_model_betas <- fixef(groom_model)$cond # get betas (effect estimates) for intercept and predictor variables 
-# effect estimates reported in Table 1  
+round(groom_model_betas, 3) # effect estimates reported in Table 1  
 
 
 #############################################################################################################################
@@ -32,4 +32,4 @@ prox_model <- glmmTMB(prox_two_month ~ assortative_genetic_ancestry_index + hete
 # look at model output (this includes non-permuted p-values)
 summary(prox_model)
 prox_model_betas <- fixef(prox_model)$cond # get betas (effect estimates) for intercept and predictor variables 
-round(prox_model_betas, 3) # effect estimates reported in Table 1  
+round(prox_model_betas, 3) # effect estimates reported in Table 2  
