d7ecce7c2a28266dc7734f9c65cea41039e9f5cd
ArielleF <afogel29@gmail.com>
2021-05-25 17:56:50 -0400

    Update README.sh

--- Permutations/README.sh
+++ Permutations/README.sh
@@ -1,8 +1,17 @@
-# module load R
-sbatch --mem=2G --cpus-per-task=20 --array=1-50%25 groom_perm.R
-sbatch --mem=2G --cpus-per-task=20 --array=1-50%25 prox_perm.R 
+# module load R before running script since we're set up the groom_perm.R and prox_perm.R to load our current environment
+sbatch --mem=100 --cpus-per-task=20 --array=1-50%25 groom_perm.R
+sbatch --mem=100 --cpus-per-task=20 --array=1-50%25 prox_perm.R 
 
-cat GLMMTMB_permuted_results_prox1* >> all.GLMMTMB_permuted_results_prox1_noC_repstate_recoded.txt
-cat GLMMTMB_permuted_results_prox2* >> all.GLMMTMB_permuted_results_prox2_noC_repstate_recoded.txt
-cat GLMMTMB_permuted_results_groom1* >> all.GLMMTMB_permuted_results_groom1_noC_repstate_recoded.txt
-cat GLMMTMB_permuted_results_groom2* >> all.GLMMTMB_permuted_results_groom2_noC_repstate_recoded.txt
+# concatenate the 50 jobs per array (20 permutations per job) into a single file for each model
+cat groom_permuted_results_* >> all.groom_permuted_results.txt
+cat prox_permuted_results_* >> all.prox_permuted_results.txt
+
+# delete individual files for each job in the array as these results are now stored in the all.xxx_permute_results.txt files above
+rm groom_permuted_results_*
+rm prox_permuted_results_*
+
+# check that each data frame with all of the permutaiton results has 1,000 lines corresponding to the 1,000 permutations run
+wc -l all.*
+#1000 all.groom_permuted_results.txt
+#1000 all.prox_permuted_results.txt
+#2000 total
