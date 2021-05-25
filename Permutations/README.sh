655d6906cef0402c56ded0019c60b90cc96c6691
ArielleF <afogel29@gmail.com>
2021-05-25 17:32:54 -0400

    Update README.sh

--- Permutations/README.sh
+++ Permutations/README.sh
@@ -1,11 +1,6 @@
 # module load R
-sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 prox_noC_repstate1_recoded_glmmtmb.R 
-
-sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 prox_noC_repstate2_recoded_glmmtmb.R 
-
-sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 groom_noC_repstate1_recoded_glmmtmb.R 
-
-sbatch --mem=2G --cpus-per-task=20 --array=1-50%5 groom_noC_repstate2_recoded_glmmtmb.R 
+sbatch --mem=2G --cpus-per-task=20 --array=1-50%25 groom_perm.R
+sbatch --mem=2G --cpus-per-task=20 --array=1-50%25 prox_perm.R 
 
 cat GLMMTMB_permuted_results_prox1* >> all.GLMMTMB_permuted_results_prox1_noC_repstate_recoded.txt
 cat GLMMTMB_permuted_results_prox2* >> all.GLMMTMB_permuted_results_prox2_noC_repstate_recoded.txt
