3aa873631223e75cdc68a9d756c9d5c6bbd19e9a
ArielleF <afogel29@gmail.com>
2021-05-27 15:16:56 -0400

    Update README.sh

--- Permutations/README.sh
+++ Permutations/README.sh
@@ -1,8 +1,11 @@
-# In R, run 1prepare_for_permutations.R which computes, for each female-two-month interval combination, the proportion of dyads where an event (grooming or proximity) occurred (these values are estimates of the probability of grooming or proximity with any male, per female-interval combination). This script generates two R data files (.Rd) which can then be uploaded to a computing cluster for parallelization of permutations.
+# In R, run 1prepare_for_permutations.R which computes, for each female-two-month interval combination, the proportion of dyads where an event (grooming or proximity) occurred (these values are estimates of the probability of grooming or proximity with any male, per female-interval combination). This script generates two R data files (groom_perm.Rd and prox_perm.Rd) which can then be uploaded to a computing cluster for parallelization of permutations.
 
-# In a directory on a computing cluster containing both R data files, run the scripts 2agroom_perm.R and 2bprox_perm.R as arrays (see below) which will randomly permute the grooming/proximity probabilities computed in 1prepare_for_permutations.R across all female two-month intervals), randomly assign 0/1 by drawing from a binomial distribution with probability equal to the permuted grooming/proximity probability for each female two-month interval, and then fit the same grooming/proximity model used to analyze the real data but using the new, permutation-based outcome variable.
-# Run 50 jobs per array since each job is currently set to run 20 permutations (can be changed by setting nperm to a value other than 20 in groom_perm.R and prox_perm.R) and we want to run 1,000 permutations per model in total (50 jobs * 20 permutations/job = 1,000 permutations total)
-# module load R before running script since we're set up the groom_perm.R and prox_perm.R to load our current environment
+# In a directory on a computing cluster containing both R data files, run the scripts 2agroom_perm.R and 2bprox_perm.R as arrays (see below) which will:
+# (a) randomly permute the grooming/proximity probabilities computed in 1prepare_for_permutations.R across all female two-month intervals, 
+# (b) randomly assign 0/1 by drawing from a binomial distribution with probability equal to the permuted grooming/proximity probability for each female two-month interval, and
+# (c) fit the same grooming/proximity model used to analyze the real data but using the new, permutation-based outcome variable.
+# Run 50 jobs per array (since each job is currently set to run 20 permutations which can be changed by setting nperm to a value other than 20 in groom_perm.R and prox_perm.R) because we want to run 1,000 permutations per model in total (50 jobs * 20 permutations/job = 1,000 permutations total)
+# module load R before running script since the groom_perm.R and prox_perm.R scripts are set up to load our current environment
 sbatch --mem=100 --cpus-per-task=20 --array=1-50%5 2agroom_perm.R
 sbatch --mem=100 --cpus-per-task=20 --array=1-50%5 2bprox_perm.R 
 
@@ -10,7 +13,7 @@ sbatch --mem=100 --cpus-per-task=20 --array=1-50%5 2bprox_perm.R
 cat groom_permuted_results_*.txt >> all.groom_permuted_results.txt
 cat prox_permuted_results_*.txt >> all.prox_permuted_results.txt
 
-# Delete individual files for each job in the array as these results are now stored in either the all.groom_permuted_results.txt or all.prox_permuted_results.txt files above
+# Delete individual files for each job in the array as these results are now stored in either the all.groom_permuted_results.txt or all.prox_permuted_results.txt files generated above
 rm groom_permuted_results_*
 rm prox_permuted_results_*
 
