6d5715157338f08a256628b43cc44f24d8afe8d7
ArielleF <afogel29@gmail.com>
2021-09-01 14:46:24 -0400

    Update 2bprox_perm.R

--- Permutations/2bprox_perm.R
+++ Permutations/2bprox_perm.R
@@ -13,8 +13,8 @@ clusterExport(clus,varlist=c("prox_null_unique_prob","prox_null_2"),envir=enviro
 nperm=20 # run 20 permutations per job in the array (and we'll run 50 arrays so in total = 50 * 20 = 1000 permutations)
 
 permuted_results=Reduce(rbind,parLapply(clus,1:nperm,function(perm){
-  library(glmmTMB, lib.loc="/data/tunglab/asf40/programs/Rlibs/") # location of installed glmmTMB library
-  prox_null_unique_prob$prox_prob_perm <- sample(prox_null_unique_prob$prox_prob,2338,replace=F) # randomly permute proximity probabilities across all female two-month intervals; note that 2338 is the number of female two-month intervals in the proximity data set
+  library(glmmTMB, lib.loc="Rlibs/") # location of installed glmmTMB library (here, it is installed in the folder Rlibs)
+  prox_null_unique_prob$prox_prob_perm <- sample(prox_null_unique_prob$prox_prob,2338,replace=F) # randomly permute proximity probabilities across all female two-month intervals; note that 2338 is the number of unique female two-month intervals in the proximity data set
   prox_null_perm <- merge(prox_null_2,prox_null_unique_prob,by="female_interval_id")
   prox_null_perm$prox_two_month_perm=NA # this is where we will put the new, permutation-based outcome variable
   for (name in prox_null_unique_prob$female_interval_id){
@@ -27,11 +27,11 @@ permuted_results=Reduce(rbind,parLapply(clus,1:nperm,function(perm){
     glmmTMB(prox_two_month_perm ~ assortative_genetic_ancestry_index + heterozygosity_female + heterozygosity_male + genetic_relatedness + rank_female*rank_male + female_age + female_age_transformed + females_in_group + males_in_group + reproductive_state*genetic_ancestry_female + reproductive_state*genetic_ancestry_male + pair_coresidency + observer_effort + (1 | female_id) + (1 | male_id), data = prox_null_perm, family = "binomial")
   
   print(perm)
-  return(unlist(fixef(prox_null_model))[1:19])
+  return(unlist(fixef(prox_null_model))[1:19]) # output intercept and all predictor variables in the model
 }))
 
 
-write.table(permuted_results,file=paste0("prox_permuted_results_",as.numeric(Sys.getenv(c("SLURM_ARRAY_TASK_ID"))),".txt"),quote=F,col.names=F,row.names=F) # output results into text file with name that identifies the job number within the array (i.e., 1-50) so that this file doesn't overwrite other job outputs from the array
+write.table(permuted_results,file=paste0("prox_permuted_results_",as.numeric(Sys.getenv(c("SLURM_ARRAY_TASK_ID"))),".txt"),quote=F,col.names=F,row.names=F) # output results as text file with name that identifies the job number within the array (i.e., 1-50) so that this file doesn't overwrite other job outputs from the array
 
 print("done")
 
